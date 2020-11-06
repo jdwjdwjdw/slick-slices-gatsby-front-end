71641f52a6ee2d87850fe7e3122fb63aa20e5107
jdwjdwjdw <jacobdeanwilson@gmail.com>
2020-11-05 22:26:23 -0800

    Added routing and navigation

--- src/pages/slicemasters.jsx
+++ src/pages/slicemasters.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from '../components/Nav';
 
 export default function SlicemastersPage() {
   return (
     <div>
+      <Nav />
       <p>Hey! I'm the Slicemasters page</p>
     </div>
   );
