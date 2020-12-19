b5e643b90c0fe8db43dca3bb49279afe02882f12
jdwjdwjdw <jacobdeanwilson@gmail.com>
2020-12-19 03:57:12 -0800

    Add GlobalStyles, Remove files from the index (now hopefully ignored)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Nav from './Nav';
 import Footer from './Footer';
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
 
 export default function Layout({ children }) {
   return (
     <div>
+      <GlobalStyles />
       <Nav />
       {children}
       <Footer />
