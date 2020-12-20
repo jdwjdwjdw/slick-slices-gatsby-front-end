f332a34c8ba6a216b2ed85a76adc42cd822a68b4
jdwjdwjdw <jacobdeanwilson@gmail.com>
2020-12-19 19:46:06 -0800

    Add custom Typography styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,11 +3,13 @@ import Nav from './Nav';
 import Footer from './Footer';
 import 'normalize.css';
 import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 
 export default function Layout({ children }) {
   return (
     <div>
       <GlobalStyles />
+      <Typography />
       <Nav />
       {children}
       <Footer />
