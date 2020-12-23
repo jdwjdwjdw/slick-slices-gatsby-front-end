6a69651d398d35c8a8a22493bee10054d05bb004
jdwjdwjdw <jacobdeanwilson@gmail.com>
2020-12-22 18:32:09 -0800

    Add gatsby-source-sanity plugin to source data from sanity back-end

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,24 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: '.env' });
+
 module.exports = {
   siteMetadata: {
     title: `Slick Slices`,
     siteUrl: `https://gatsby.pizza`,
     description: `The best pizza place in Hamilton!`,
   },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      // this is the mame of the plugin you are adding
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'xdisyh4e',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
 };
