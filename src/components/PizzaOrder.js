3ddfc76fc5d3e082cd93f747390587d9db3ea3c5
jdwjdwjdw <jacobdeanwilson@gmail.com>
2020-12-30 03:48:17 -0800

    Fix issue with adding and deleting pizzas due to duplicate keys

--- src/components/PizzaOrder.js
+++ src/components/PizzaOrder.js
@@ -8,9 +8,11 @@ export default function PizzaOrder({ order, pizzas, removeFromOrder }) {
   return (
     <>
       {order.map((singleOrder, index) => {
-        const pizza = pizzas.find((pizza) => pizza.id === singleOrder.id);
+        const pizza = pizzas.find(
+          (singlePizza) => singlePizza.id === singleOrder.id
+        );
         return (
-          <MenuItemStyles key={singleOrder.id}>
+          <MenuItemStyles key={`${singleOrder.id}-${index}`}>
             <Img fluid={pizza.image.asset.fluid} />
             <h2>{pizza.name}</h2>
             <p>
