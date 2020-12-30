e79aecb4f908e42c46cf64b22f5d485ef33edfff
jdwjdwjdw <jacobdeanwilson@gmail.com>
2020-12-29 23:37:17 -0800

    Add ability to add and remove orders on the order page

--- src/pages/order.js
+++ src/pages/order.js
@@ -7,13 +7,19 @@ import calculatePizzaPrice from '../utils/calculatePizzaPrice';
 import formatMoney from '../utils/formatMoney';
 import OrderStyles from '../styles/OrderStyles';
 import MenuItemStyles from '../styles/MenuItemStyles';
+import usePizza from '../utils/usePizza';
+import PizzaOrder from '../components/PizzaOrder';
 
 export default function OrderPage({ data }) {
+  const pizzas = data.pizzas.nodes;
   const { values, updateValue } = useForm({
     name: '',
     email: '',
   });
-  const pizzas = data.pizzas.nodes;
+  const { order, addToOrder, removeFromOrder } = usePizza({
+    pizzas,
+    inputs: values,
+  });
   return (
     <>
       <SEO title="Order a Pizza!" />
@@ -56,7 +62,16 @@ export default function OrderPage({ data }) {
               </div>
               <div>
                 {['S', 'M', 'L'].map((size) => (
-                  <button key={size} type="button">
+                  <button
+                    key={size}
+                    type="button"
+                    onClick={() =>
+                      addToOrder({
+                        id: pizza.id,
+                        size,
+                      })
+                    }
+                  >
                     {size} {formatMoney(calculatePizzaPrice(pizza.price, size))}
                   </button>
                 ))}
@@ -66,6 +81,11 @@ export default function OrderPage({ data }) {
         </fieldset>
         <fieldset className="order">
           <legend>Order</legend>
+          <PizzaOrder
+            order={order}
+            removeFromOrder={removeFromOrder}
+            pizzas={pizzas}
+          />
         </fieldset>
       </OrderStyles>
     </>
---
+++ src/utils/usePizza.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+export default function usePizza({ pizzas, inputs }) {
+  // 1. Create some state to hold our order
+  const [order, setOrder] = useState([]);
+  // 2. Make a function to add things to order
+  function addToOrder(orderedPizza) {
+    setOrder([...order, orderedPizza]);
+  }
+  // 3. Make a function to remove things from order
+  function removeFromOrder(index) {
+    setOrder([
+      // everything before the item we want to remove
+      ...order.slice(0, index),
+      // everything after the item we want to remove
+      ...order.slice(index + 1),
+    ]);
+  }
+  // 4. Send this data to a serverless function when they check out
+  // TODO
+
+  return {
+    order,
+    addToOrder,
+    removeFromOrder,
+  };
+}
