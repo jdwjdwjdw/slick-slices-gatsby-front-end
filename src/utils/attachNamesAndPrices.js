b466cef67b42b4286f663516a54db69e5b6deed7
jdwjdwjdw <jacobdeanwilson@gmail.com>
2020-12-30 21:48:33 -0800

    Validate code and send it off to email, fix eslint issue

--- src/utils/attachNamesAndPrices.js
+++ src/utils/attachNamesAndPrices.js
@@ -3,7 +3,7 @@ import formatMoney from './formatMoney';
 
 export default function attachNamesAndPrices(order, pizzas) {
   return order.map((item) => {
-    const pizza = pizzas.find((pizza) => pizza.id === item.id);
+    const pizza = pizzas.find((singlePizza) => singlePizza.id === item.id);
     return {
       ...item,
       name: pizza.name,
